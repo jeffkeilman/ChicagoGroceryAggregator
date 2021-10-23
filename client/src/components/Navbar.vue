9030245924c850be2cade69e8ab7043b1c8f514e
Sarah Tennis <sarah.n.tennis@gmail.com>
2021-10-22 22:38:38 -0500

    Create Vue CLI project, inject scss variables into every component

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav>
+    
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+nav {
+  height: 50px;
+  width: 100vw;
+  background: green;
+}
+</style>
